0fe07a0f88d7676244fd97c17b6fad8dd4a25ac0
oaugusto <oaugusto@outlook.com>
2019-09-11 18:08:41 -0300

    Scripts for simulation

---
+++ scripts/randomwalk.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+$project="$ARGV[0]";
+$input="input/randomwalkDS";
+$output="output/$project/randomwalkDS";
+$numNodes = 128; # number of rounds to perform per simulation
+$numSimulations = $ARGV[1];
+
+@walks = (4, 16, 32);
+@flows = (1, 4, 16);
+
+for($g = 1; $g <= $numSimulations; $g+=1) {
+    for($numNodes = 128; $numNodes <= 1024; $numNodes*=8) {
+        for my $i (@walks) {
+            for my $j (@flows) {
+                print "java -cp binaries/bin:binaries/jdom.jar sinalgo.Run -batch " .
+                "-project $project " .                                                      
+                "-overwrite input=$input/$numNodes-nodes-$i-walkLength-$j-numberOfConcurrentFlows-input.txt output=$output/$numNodes/$i/$j/$g " .  
+                "AutoStart=true\n";
+
+                system("java -cp binaries/bin:binaries/jdom.jar sinalgo.Run -batch " .
+                "-project $project " .                                                      
+                "-overwrite input=$input/$numNodes-nodes-$i-walkLength-$j-numberOfConcurrentFlows-input.txt output=$output/$numNodes/$i/$j/$g " .  
+                "AutoStart=true > /dev/null");                                              
+            }
+        }
+    }
+}
