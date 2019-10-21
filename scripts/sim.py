2fcbd7f205741bc10729ff57aa10de47236db29b
oaugusto <oaugusto@outlook.com>
2019-10-21 09:57:30 -0300

    update sim.py

--- scripts/sim.py
+++ scripts/sim.py
@@ -2,6 +2,23 @@
 
 import sys
 import os
+import threading
+import numpy
+
+# this file keep all completed experiments
+log_file = "scripts/logs/log.txt"
+
+if not os.path.exists(log_file):
+    os.mknod(log_file)
+
+# read log file
+log = set(line.rstrip() for line in open(log_file))
+
+# open log file for append and create a lock variable
+file = open("scripts/logs/log.txt", "a+")
+file_lock = threading.Lock()
+
+projects = ["cbnet", "seqcbnet", "splaynet", "displaynet", "semisplaynet", "seqsemisplaynet", "simplenet", "optnet"]
 
 # sinalgo configuration
 java = 'java'
@@ -14,9 +31,40 @@ command = '{} -cp {} {} {}'.format(java, classpath, program, ' '.join(args))
 inp = sys.argv[1]
 out = sys.argv[2]
 
-cmd = '{} {} -overwrite input={} output={} AutoStart=true > /dev/null'.format(command, project, inp, out)
+#extends thread class
+class myThread (threading.Thread):
+    def __init__(self, threadID, command):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.command = command
+    def run(self):
+        execute(self.command)
 
-#execute the cmd
-os.system(cmd)
 
-print("Simulation completed:\n" + cmd + "\n")
+def execute(command):
+    os.system(command)
+
+    file_lock.acquire()
+    file.write(command + "\n")
+    file_lock.release()
+
+threads = []
+threadID = 1
+
+#for each project executed
+for project in projects:
+    
+    cmd = '{} {} -overwrite input={} output={} AutoStart=true > /dev/null'.format(command, project, inp, project + "/" + out)
+    
+    # not executed yet
+    if cmd not in log:
+        thread = myThread(threadID, cmd)
+        thread.start()
+        threads.append(thread)
+        threadID += 1
+
+# Wait for all threads to complete
+for t in threads:
+    t.join()
+
+print("Simulation completed\n")
