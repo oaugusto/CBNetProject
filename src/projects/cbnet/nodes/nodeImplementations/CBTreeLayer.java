78d0b90218be40c0843438d705f4670dc1562d77
oaugusto <oaugusto@outlook.com>
2020-02-02 20:13:31 -0300

    update counter on tree until the root

--- src/projects/cbnet/nodes/nodeImplementations/CBTreeLayer.java
+++ src/projects/cbnet/nodes/nodeImplementations/CBTreeLayer.java
@@ -31,28 +31,29 @@ public abstract class CBTreeLayer extends BinarySearchTreeLayer {
 
 
     // TODO : change the way weights are updated
+    // now update til the root
     public void updateWeights(int from, int to) {
         CBTreeLayer aux = this;
 
-        if (!aux.isAncestorOf(to)) {
-            while (!aux.isAncestorOf(to)) {
+        if (!(aux.getParent() == null)) {
+            while (!(aux.getParent() == null)) {
                 aux.incrementWeight();
                 aux = (CBTreeLayer) aux.getParent();
-			} 
-			
-			aux.incrementWeight();
+            }
+
+            aux.incrementWeight();
         }
 
         while (aux.ID != to) {
-			aux.incrementWeight();
-			if (aux.ID > to) {
-				aux = (CBTreeLayer) aux.getLeftChild();
-			} else {
-				aux = (CBTreeLayer) aux.getRightChild();
-			}
-		}
+          aux.incrementWeight();
+          if (aux.ID > to) {
+            aux = (CBTreeLayer) aux.getLeftChild();
+          } else {
+            aux = (CBTreeLayer) aux.getRightChild();
+          }
+        }
 		
-		aux.incrementWeight();
+		    aux.incrementWeight();
     }
 
     @Override
